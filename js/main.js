bdb5b2375ab41fda9c2730b821bd882a7c8f20f4
Russ Perlow <rep4975@rit.edu>
2018-04-25 23:48:14 -0400

    Playlists are populated upon first open

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import {search} from './lyrics.js';
-import {spotifyInit, requestAuthorization, getPlaylist, playDemo} from './spotify.js';
+import {spotifyInit, requestAuthorization, getPlaylist, playDemo, getPlaylistSongs} from './spotify.js';
 export {init};
 
 // window.onload = init();
@@ -21,28 +21,42 @@ Vue.component('playlist-list', {
 
 Vue.component('playlist-nested-song',{
     props:['song', 'index'],
-    template:   `<b-card>{{song.title}}</b-card>`
+    template:   `<b-card>
+                    <button v-on:click="songClick">{{song.title}}</button>
+                </b-card>`,
+    methods: {
+        songClick: function(event){
+            console.log("Event");
+        }
+    }        
 });
 
 Vue.component('playlist-nested-collapse', {
     props:['list', 'index'],
-    template:   `<b-card>
-                    <b-btn v-b-toggle="list.id">{{list.name}}</b-btn>
+    template:   `<b-card class="playlist-nested-card">
+                    <b-btn v-b-toggle="list.id" :id="list.id"class="playlist-nested" v-on:click="playlistClick">{{list.name}}</b-btn>
                     <b-collapse :id="list.id">
                         <b-card is="playlist-nested-song" v-for="(song, index2) in list.songs" v-bind:song="song" v-bind:index="index2">
                         </b-card>
                     </b-collapse>
-                </b-card>`
+                </b-card>`,
+    methods:{
+        playlistClick: function(event){
+            getPlaylistSongs(event.target.id);
+        }
+    }
 });
 
 Vue.component('playlist-collapse', {
     props:['lists', 'title'],
     template:   `<div>
-                    <b-btn v-b-toggle.collapse1 variant="secondary">Playlists</b-btn>
+                <b-card id="playlist-collapse-card">
+                    <b-btn v-b-toggle.collapse1 id="playlist-collapse">Playlists</b-btn>
                     <b-collapse id="collapse1" class="mt-2">
                         <b-card is="playlist-nested-collapse" v-for="(list, index) in lists" v-bind:list="list" v-bind:index="index">
                         </b-card>
                     </b-collapse>
+                </b-card>
                 </div>`
 });
 
@@ -66,12 +80,17 @@ let app = new Vue({
     }
 });
 
+
 function init(){
     document.querySelector("#search").onclick = function(event){search();}
     document.querySelector("#playDemoBtn").onclick = function(event){playDemo();}
 
     let playlistBtn = document.querySelector("#getPlaylistsBtn");
-    playlistBtn.onclick = function(event){app.loadPlaylists(getPlaylist());};
+    playlistBtn.onclick = function(event){
+        getPlaylist(function(playlists){
+            app.loadPlaylists(playlists);
+        });
+    };
 
     spotifyInit();
 }
--- js/spotify.js
+++ js/spotify.js
@@ -1,13 +1,15 @@
-export {spotifyInit, requestAuthorization, getPlaylist, playDemo};
+export {spotifyInit, requestAuthorization, getPlaylist, playDemo, getPlaylistSongs};
 "use strict";
 
 const URL_AUTHORIZE = '';
-const SPOTIFY_PLAYER_ACCESS_TOKEN = "BQAdQGXq-Ks6uQQ4Ba1FjvTrvXB_RD0dVxMkket7Foouj4JbbVhW5TAXSX9xJP4ESDhm7cSUxK-fFlFTcBsE7drdRkhcjiO_WCMeojAHMGjoCN7xOPQfN45OandigyHbDarDxKnAfJ4Zs0pbWblzqemWlP4h6x8ukrW5ow";
+const SPOTIFY_PLAYER_ACCESS_TOKEN = "BQAPZHccI5MqUYSeT4CTD2PccgNalIC5A3CI92y56Fo-9fnJ7w7q83Pbs5YhN1ZqGuBYMYzUWZ88p0tBH5JcyJzFspv3cPOMa60rTZtwBu6lCA8OtlLRiOKhKdNfXMH00OpghJXqIE4yjAUhFowu_yn-Brcjvbg1EXWS3g"; // Special Playback SDK id
 let accessToken = " ";
 let redirect_uri = "https://people.rit.edu/rep4975/330/HowsItGo/main.html";
-let client_id = "27a24f33f6b8467991e0b78665a190e2";
+let client_id = "27a24f33f6b8467991e0b78665a190e2"; // My client ID for authorization
 let URL = "";
-let deviceId = "47e5af3c94c7031ee18be0132c472e28a020e7d2";
+let deviceId = "47e5af3c94c7031ee18be0132c472e28a020e7d2"; // id of the device this is running on 
+let user_id = ""; // The id of the user who signed in
+let display_name = ""; // The display name of the user who signed in
 let playlists = [];
 let names = [];
 
@@ -22,6 +24,8 @@ function spotifyInit(){
         temp = temp.split('&')[0];
         accessToken = temp;
         console.log("Received spotify access token: " + accessToken);
+
+        accessSpotifyAPI();
     }
     else if(currentURL.includes("error")){
         let temp = currentURL.split('=');
@@ -37,7 +41,7 @@ function spotifyInit(){
 window.onSpotifyWebPlaybackSDKReady = () => {
 
     // ONLY FOR WHEN TESTING WITHOUT AUTHENTICATION KEY
-    return;
+    // return;
 
     const token = SPOTIFY_PLAYER_ACCESS_TOKEN;
     const player = new Spotify.Player({
@@ -70,13 +74,13 @@ function requestAuthorization(){
 }
 
 // Gets playlists from Spotify
-function loadPlaylists(){
+function loadPlaylists(callback){
 
     // TESTING PURPOSES ONLY
-    playlists.push(new Playlist("link1", "342lnf423q", "Drake v. Meek"));
-    playlists.push(new Playlist("link2", "gsfdgsdh43", "Chill"));
-    playlists.push(new Playlist("link3", "h5634jjehg", "Music TBT"));
-    return;
+    // playlists.push(new Playlist("link1", "342lnf423q", "Drake v. Meek"));
+    // playlists.push(new Playlist("link2", "gsfdgsdh43", "Chill"));
+    // playlists.push(new Playlist("link3", "h5634jjehg", "Music TBT"));
+    // return;
 
     $.ajax({
         url: 'https://api.spotify.com/v1/me/playlists',
@@ -86,20 +90,61 @@ function loadPlaylists(){
         success: function(response){
             let items = response.items;
             for(let i = 0; i < items.length; i++){
-                playlists.push(items[i].href, items[i].id, items[i].name);
+                playlists.push(new Playlist(items[i].href, items[i].id, items[i].name));
                 names.push(items[i].name);
             }
+            callback(playlists);
         }
     });
 }
 
 // Returns the playlists that have been loaded
-function getPlaylist(){
-    if(playlists.length == 0)
-        loadPlaylists();
-    return playlists;
-    // names = ["Drake v. Meek", "Chill", "Music TBT"];
-    // return names;
+function getPlaylist(callback){
+    if(playlists.length == 0){
+        loadPlaylists(callback);
+    }
+    else{
+        callback(playlists);
+    }
+    // return playlists;
+}
+
+function getPlaylistSongs(playlist_id){
+    let currentPlaylist;
+
+    for(let i = 0; i < playlists.length; i++){
+        if(playlists[i].id == playlist_id){
+            currentPlaylist = playlists[i];
+            break;
+        }
+    }
+
+    $.ajax({
+        url: 'https://api.spotify.com/v1/users/' + user_id + "/playlists/" + playlist_id + "/tracks",
+        headers: {
+            'Authorization': 'Bearer ' + accessToken
+        },
+        success: function(response){
+            populatePlaylist(response, currentPlaylist);
+        }
+    });
+}
+
+function populatePlaylist(obj, currentPlaylist){
+    let items = obj.items;
+    let songs = []
+    for(let i = 0; i < items.length; i++){
+        let track = items[i].track;
+
+        let album = track.album.name;
+        let artist = track.artists[0].name;
+        let id = track.id;
+        let link = track.href;
+        let title = track.name;
+
+        songs.push(new Song(link, id, title, artist, album));
+    }
+    currentPlaylist.loadSongs(songs);
 }
 
 // General function for accessing spotify api
@@ -110,7 +155,8 @@ function accessSpotifyAPI(){
             'Authorization': 'Bearer ' + accessToken
         },
         success: function(response){
-            console.log("success");
+            user_id = response.id;
+            display_name = response.display_name;
         }
     });
 }
@@ -146,7 +192,12 @@ class Playlist{
         this.link = link;
         this.id = id;
         this.name = name;
-        this.songs = [new Song("link1", "id123", "Title 1", "Artist 1", "Album 1"), new Song("link2", "id456", "Title 2", "Artist 2", "Album 2")];
+        this.songs = [];
+        // this.songs = [new Song("link1", "id123", "Title 1", "Artist 1", "Album 1"), new Song("link2", "id456", "Title 2", "Artist 2", "Album 2")];
+    }
+
+    loadSongs(songs){
+        this.songs = songs;
     }
 }
 
